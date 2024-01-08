3c0d41dc339c6afc638d1b5ccbfc42a3a005d092
heyimaaditya <aadityatyagi158@gmail.com>
2024-01-08 16:54:36 +0530

    create react app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-dark">
+      <h1 className='text-danger'>Hello world</h1>
+    </div>
+      
+  );
+}
+
+export default App;

e8be2c9f25d6d052bf2263762e81be7c4a9eb208
heyimaaditya <aadityatyagi158@gmail.com>
2024-01-08 17:01:49 +0530

    all pages initialise

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import Register from './pages/Register';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="bg-dark">
-      <h1 className='text-danger'>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
       
   );
 }

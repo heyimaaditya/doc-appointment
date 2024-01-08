01ab0abed39d84c2f0adb9274bf28f83d8ed9a4c
heyimaaditya <aadityatyagi158@gmail.com>
2024-01-08 17:16:04 +0530

    login and registeration form done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
-const Login = () => {
+import { Form,Input } from 'antd';
+import { Link } from 'react-router-dom';
+import '../styles/RegisterStyles.css'
+const Login=()=>{
+  const onfinishHandler=(values)=>{
+    console.log(values);
+  }
   return (
-    <div>Login</div>
+    <>
+      <div className='form-container'>
+        <Form className='register-form' layout="vertical" onFinish={onfinishHandler}>
+          <h3 className='text-center'>Login Form</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required/> 
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required/> 
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required/> 
+          </Form.Item>
+          <Link to='/register' className='m-2'>{''}Not a user?go for register</Link>
+          <button className='btn btn-primary' type='submit'>Login</button>
+
+        </Form>
+      </div>
+    </>
   )
 }
-
-export default Login
+export default Login;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
-const Register = () => {
+import {Form,Input} from 'antd';
+import { Link } from 'react-router-dom';
+import '../styles/RegisterStyles.css'
+const Register=()=>{
+  const onfinishHandler=(values)=>{
+    console.log(values);
+  }
   return (
-    <div>Register</div>
+    <>
+      <div className='form-container'>
+        <Form className='register-form' layout="vertical" onFinish={onfinishHandler}>
+          <h3 className='text-center'>Register Form</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required/> 
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required/> 
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required/> 
+          </Form.Item>
+          <Link to='/login' className='m-2'>{''}Already Registered?go for login</Link>
+          <button className='btn btn-primary' type='submit'>Register</button>
+
+        </Form>
+      </div>
+    </>
   )
 }
-
-export default Register
+export default Register;
